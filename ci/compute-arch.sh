e9c23175dd46ff3a9a30c5b94366e8acb0a2a3ca
AJ Schmidt <aschmidt@nvidia.com>
2022-12-08 12:27:17 -0500

    add script to compute image architectures

---
+++ ci/compute-arch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script to determine which image architectures should
+# be built for different CUDA/OS variants.
+# Example Usage:
+#   CUDA_VER=11.5.1 LINUX_VER=centos8 ./ci/compute-arch.sh
+set -eu
+
+PLATFORMS="linux/amd64"
+
+write_platforms() {
+  local PLATFORMS="${1}"
+
+  # Use /dev/null to ensure the script can be tested locally
+  echo "PLATFORMS=${PLATFORMS}" | tee --append "${GITHUB_OUTPUT:-/dev/null}"
+}
+
+# Ubuntu18.04 images don't officially support arm.
+# Even though Ubuntu18.04 images prior to CUDA 11.8.0 did
+# have arm variants, it was removed for 11.8.0.
+if [[
+  "${CUDA_VER}" == "11.8.0" &&
+  "${LINUX_VER}" == "ubuntu18.04"
+]]; then
+  write_platforms "${PLATFORMS}"
+  exit 0
+fi
+
+if [[
+  "${CUDA_VER}" > "11.2.2" &&
+  "${LINUX_VER}" != "centos7"
+]]; then
+  PLATFORMS+=",linux/arm64"
+fi
+
+write_platforms "${PLATFORMS}"
