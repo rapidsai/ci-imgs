f1677641bc28721e65b9c95d65fe148b3eba9498
Jake Awe <jawe@nvidia.com>
2023-09-22 06:00:33 -0500

    remove rapidsai/ci references

--- ci/remove-temp-images.sh
+++ ci/remove-temp-images.sh
@@ -26,17 +26,3 @@ for arch in $(echo "$ARCHES" | jq .[] -r); do
     -H "Authorization: JWT $HUB_TOKEN" \
     "https://hub.docker.com/v2/repositories/$full_repo_name/tags/$tag-$arch/"
 done
-
-# delete rapidsai/ci imgs too
-if [[ $IMAGE_NAME =~ ci-conda ]]; then
-  IMAGE_NAME=$(echo "$IMAGE_NAME" | sed 's/ci-conda/ci/')
-  full_repo_name=${IMAGE_NAME%%:*}
-  tag=${IMAGE_NAME##*:}
-
-  for arch in $(echo "$ARCHES" | jq .[] -r); do
-    curl --fail-with-body -i -X DELETE \
-      -H "Accept: application/json" \
-      -H "Authorization: JWT $HUB_TOKEN" \
-      "https://hub.docker.com/v2/repositories/$full_repo_name/tags/$tag-$arch/"
-  done
-fi
