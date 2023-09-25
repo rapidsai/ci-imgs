db8d7f92cbc9a08ed057f14f24a79a4755ade361
Jake Awe <50372925+AyodeAwe@users.noreply.github.com>
2023-09-25 12:23:26 -0500

    remove rapidsai/ci references (#74)

--- ci/create-multiarch-manifest.sh
+++ ci/create-multiarch-manifest.sh
@@ -31,29 +31,3 @@ if [[
     echo "Skipping 'latest' manifest creation for PR workflow."
   fi
 fi
-
-# publish rapidsai/ci manifests too
-if [[ $IMAGE_NAME =~ ci-conda ]]; then
-  IMAGE_NAME=$(echo "$IMAGE_NAME" | sed 's/ci-conda/ci/')
-  source_tags=()
-  tag="${IMAGE_NAME}"
-  for arch in $(echo "${ARCHES}" | jq .[] -r); do
-    source_tags+=("${tag}-${arch}")
-  done
-
-  docker manifest create "${tag}" "${source_tags[@]}"
-  docker manifest push "${tag}"
-  if [[
-    "${LATEST_UBUNTU_VER}" == "${LINUX_VER}" &&
-    "${LATEST_CUDA_VER}" == "${CUDA_VER}" &&
-    "${LATEST_PYTHON_VER}" == "${PYTHON_VER}"
-  ]]; then
-    # only create a 'latest' manifest if it is a non-PR workflow.
-    if [[ "${BUILD_TYPE}" != "pull-request" ]]; then
-      docker manifest create "rapidsai/ci:latest" "${source_tags[@]}"
-      docker manifest push "rapidsai/ci:latest"
-    else
-      echo "Skipping 'latest' manifest creation for PR workflow."
-    fi
-  fi
-fi
