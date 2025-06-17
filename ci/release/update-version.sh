a1514ec62a16529a08c682dd684230e7e9e1a0b3
Bradley Dice <bdice@bradleydice.com>
2025-06-17 12:11:03 -0500

    Shellcheck for update-version.sh.

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -12,10 +12,5 @@ set -e
 # Format is YY.MM.PP - no leading 'v' or trailing 'a'
 NEXT_FULL_TAG=$1
 
-# Get <major>.<minor> for next version
-NEXT_MAJOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[1]}')
-NEXT_MINOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[2]}')
-NEXT_SHORT_TAG=${NEXT_MAJOR}.${NEXT_MINOR}
-
 echo "Updating to $NEXT_FULL_TAG"
 echo "${NEXT_FULL_TAG}" > VERSION
