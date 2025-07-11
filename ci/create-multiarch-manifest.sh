df5652c6b90e5f97964e9748106e73a910901af6
Ben Jarmak <104460670+jarmak-nv@users.noreply.github.com>
2025-07-11 14:08:39 -0400

    Add some compliance items to the repo (#284)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- ci/create-multiarch-manifest.sh
+++ ci/create-multiarch-manifest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
 
 RAPIDS_VERSION_MAJOR_MINOR=$(rapids-version-major-minor)
